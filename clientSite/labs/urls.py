5a2dd87eea790653a88bb736277dcca12c76352c
Laplas <61288992+Laplas00@users.noreply.github.com>
2025-05-26 15:38:35 +0300

    debug

--- clientSite/labs/urls.py
+++ clientSite/labs/urls.py
@@ -1,20 +1,12 @@
 from django.urls import path
-from . import views, toolkit_for_labs
+from . import views
+from . import toolkit_for_labs as labs 
 
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('modules', views.modules, name='modules'),
-    path('sql_classic', views.sql_classic, name='sql_classic'),
-    path('start_lab_for_user', toolkit_for_labs.start_lab_for, name='start_lab_for_user'),
-]
-
-
-from django.urls import path
-from . import toolkit_for_labs as labs
-
-urlpatterns = [
-    path('lab/sql_injection_classic/start', labs.start_lab, name="start_lab"),
-    path('lab/sql_injection_classic/stop', labs.stop_lab, name="stop_lab"),
+    path('start_lab', labs.start_lab, name="start_lab"),
+    path('stop_lab', labs.stop_lab, name="stop_lab"),
 ]
 
