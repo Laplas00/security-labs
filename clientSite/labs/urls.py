babce171cf660d902f55102eb22c74e59dd6a1e7
Laplas <61288992+Laplas00@users.noreply.github.com>
2025-05-26 15:44:19 +0300

    fixes

--- clientSite/labs/urls.py
+++ clientSite/labs/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from . import views
+from . import views, toolkit_for_labs
 from . import toolkit_for_labs as labs 
-
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('modules', views.modules, name='modules'),
+    path('sql_classic', views.sql_classic, name='sql_classic'),
     path('start_lab', labs.start_lab, name="start_lab"),
     path('stop_lab', labs.stop_lab, name="stop_lab"),
 ]
