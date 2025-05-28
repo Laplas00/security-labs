7fbce148b27103d3b4bd87565cc30f5209407b12
Laplas <61288992+Laplas00@users.noreply.github.com>
2025-05-28 20:50:13 +0300

    n

--- clientSite/labs/views.py
+++ clientSite/labs/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from .toolkit_for_labs import generate_lab_token,  get_lab_status_for_user
+from .toolkit_for_labs import generate_lab_token,  get_lab_status
 
 
 @login_required
@@ -16,7 +16,7 @@ def sql_classic(request):
     lab_name = "sql_inj_classic"
     user = request.user.username
     token = generate_lab_token(user, lab_name)
-    status = get_lab_status_for_user(request.user, "sql_inj_classic", token)
+    status = get_lab_status(request.user, "sql_inj_classic", token)
     return render(request, 'labs/sql_inj_classic.html', 
                   context={'lab_name':lab_name,
                            'status':status})
