b27d1f88abf4630fc1e190a9d53797ebac5e7aa6
Laplas <61288992+Laplas00@users.noreply.github.com>
2025-06-02 22:26:09 +0300

    n

--- vps-labs-api/labs/web/sql_inj_classic/app.py
+++ vps-labs-api/labs/web/sql_inj_classic/app.py
@@ -9,5 +9,5 @@ def get_db():
 if __name__ == '__main__':
     if not os.path.exists('blog.db'):
         import db_init  # инициализация базы при первом запуске
-    app.run(host='0.0.0.0', port=8000) # for traefic use 5000
+    app.run(host='0.0.0.0', port=5000) # for traefic use 5000
 
