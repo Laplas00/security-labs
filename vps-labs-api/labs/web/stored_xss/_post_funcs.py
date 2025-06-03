4f1268d5c9b0906165836ac633cfec8706c64c2b
Laplas <61288992+Laplas00@users.noreply.github.com>
2025-06-03 15:56:39 +0300

    ffix

--- vps-labs-api/labs/web/stored_xss/_post_funcs.py
+++ vps-labs-api/labs/web/stored_xss/_post_funcs.py
@@ -14,31 +14,26 @@ def post(post_id):
 @app.route("/store_message", methods=["POST"])
 def store_user_message():
     message = request.form.get("message", "").strip()
-
-    if VULNERABLE:
-        # если включена уязвимость, допустим всё (в т.ч. XSS)
-        valid = True
-    else:
-        # иначе, отфильтруем теги (наивная проверка)
+    vuln_mode = os.environ.get("VULNERABLE", "0") == "1"
+    print('vuln mode in store messages:', vuln_mode)
+    if False == vuln_mode:
         if "<script" in message.lower() or "</" in message.lower():
             flash("HTML tags are not allowed (vuln off).", "error")
             return redirect(url_for("discussion"))
-        valid = True
 
-    if valid:
-        conn = sqlite3.connect('blog.db')
-        c = conn.cursor()
-        c.execute("INSERT INTO comments (message) VALUES (?)", (message,))
-        conn.commit()
-        conn.close()
-        flash("Message posted successfully!", "success")
+    conn = sqlite3.connect('blog.db')
+    c = conn.cursor()
+    c.execute("INSERT INTO comments (message) VALUES (?)", (message,))
+    conn.commit()
+    conn.close()
+    flash("Message posted successfully!", "success")
 
     return redirect(url_for("discussion"))
 
 @app.route("/discussion", methods=["GET"])
 def discussion():
     vuln_mode = os.environ.get("VULNERABLE", "0") == "1"
-
+    print('vuln_mode', vuln_mode)
     conn = sqlite3.connect('blog.db')
     c = conn.cursor()
     c.execute("SELECT message FROM comments ORDER BY id DESC LIMIT 50")
