61d9ed539ecd5fe4e8ae7f79fcb6cbcc179ae9e8
Laplas <61288992+Laplas00@users.noreply.github.com>
2025-06-03 12:53:54 +0300

    n

---
+++ vps-labs-api/labs/web/reflected_xss/app.py
@@ -0,0 +1,12 @@
+
+from core import *
+
+
+
+if __name__ == '__main__':
+    if not os.path.exists('blog.db'):
+        import db_init  # инициализация базы при первом запуске
+
+
+    app.run(host='0.0.0.0', port=5000) # for traefic use 5000
+
