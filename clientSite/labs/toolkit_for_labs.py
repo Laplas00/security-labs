a8e89f429aa33c6a3b0264158ac739092335719c
Laplas <61288992+Laplas00@users.noreply.github.com>
2025-06-05 18:14:29 +0300

    n

--- clientSite/labs/toolkit_for_labs.py
+++ clientSite/labs/toolkit_for_labs.py
@@ -63,10 +63,10 @@ def start_lab(request):
 
     try:
         r = requests.post(f"{EDGE_IP}/start_lab", json=data)
-        ic(r)
+        print(r)
         return JsonResponse(r.json())
     except Exception as e:
-        ic(e)
+        print(e)
         return JsonResponse({"error": str(e)})
 
 @login_required
