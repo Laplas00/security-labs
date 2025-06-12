fe4bc8745d9b3d9eaac0d2fb890a594db546e6af
Laplas <61288992+Laplas00@users.noreply.github.com>
2025-06-12 20:16:13 +0300

    new

--- vps-labs-api/blog_site/run.py
+++ vps-labs-api/blog_site/run.py
@@ -4,11 +4,14 @@ from app.routes.auth_funcs import *
 from app.routes.posts import *
 from app.routes.search import *
 from app.routes.settings import *
+from app.utils.vulns import get_vuln_flags
+
 
 if __name__ == '__main__':
     if not os.path.exists('blog.db'):
         import db_init  # инициализация базы при первом запуске
 
-
+    flags = get_vuln_flags()
+    print('FLAGS:', flags)
     app.run(host='0.0.0.0', port=5000, debug=True) # for traefic use 5000
 
