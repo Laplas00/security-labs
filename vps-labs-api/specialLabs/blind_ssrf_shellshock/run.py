9e371e24345fb370e4a05c8b486d54e086879aea
Laplas <61288992+Laplas00@users.noreply.github.com>
2025-06-22 20:48:47 +0300

    n

--- vps-labs-api/specialLabs/blind_ssrf_shellshock/run.py
+++ vps-labs-api/specialLabs/blind_ssrf_shellshock/run.py
@@ -20,4 +20,4 @@ if __name__ == '__main__':
         t = threading.Thread(target=run_internal_api, daemon=True)
         t.start()
 
-    app.run(host='0.0.0.0', port=8000, debug=True) 
+    app.run(host='0.0.0.0', port=8000, use_reloader=True, debug=False) 
