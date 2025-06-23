175562a2bf6054851859b1f960bbe21872ee801a
Laplas <61288992+Laplas00@users.noreply.github.com>
2025-06-23 18:24:03 +0300

    new

--- vps-labs-api/specialLabs/ssrf_whitelist_based_bypass/app/utils/vulns.py
+++ vps-labs-api/specialLabs/ssrf_whitelist_based_bypass/app/utils/vulns.py
@@ -1,9 +1,7 @@
 import os
 
 def get_vuln_flag():
-    # flag = os.getenv("vulnerability", "")
-    flag = 'ssrf_blacklist_based_bypass'
-    flag = ''
+    flag = os.getenv("vulnerability", "")
     return flag 
 
 
