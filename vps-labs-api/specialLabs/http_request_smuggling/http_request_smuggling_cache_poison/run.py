4fb73dde2358326c9d0808b8cdb7e4f2dbc60cef
Laplas <61288992+Laplas00@users.noreply.github.com>
2025-07-05 14:38:04 +0300

    port change for nginx

--- vps-labs-api/specialLabs/http_request_smuggling/http_request_smuggling_cache_poison/run.py
+++ vps-labs-api/specialLabs/http_request_smuggling/http_request_smuggling_cache_poison/run.py
@@ -7,7 +7,7 @@ from app.routes.settings import *
 from app.utils.vulns import get_vuln_flag
 
 def run_main_app():
-    app.run(host='0.0.0.0', port=8000, debug=False, use_reloader=False)
+    app.run(host='0.0.0.0', port=5000, debug=False, use_reloader=False)
 
 if __name__ == '__main__':
     if not os.path.exists('blog.db'):
