cd5bc9ca953c0b0cfb5d98d8b1d90327d25316c5
Laplas <61288992+Laplas00@users.noreply.github.com>
2025-07-07 21:16:41 +0300

    fix

--- vps-labs-api/coreBlog/app/utils/vulns.py
+++ vps-labs-api/coreBlog/app/utils/vulns.py
@@ -4,6 +4,6 @@ def get_vuln_flag():
     flag = os.getenv("vulnerability", "")
     # return 'sql_union_column_number_discovery'
     # return 'blind_sql_injection_conditional'
-    return 'blind_sql_injection_time_delay'
+    # return 'blind_sql_injection_time_delay'
     return flag 
 
